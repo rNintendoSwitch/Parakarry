c5a5e80ea68fae124b1860a59d1924665b4fd121
Matthew Cohen <matt@mattbsg.xyz>
2020-05-06 23:47:47 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+# Discord
+token = 'discord token'
+
+#Mongo Credentials
+mongoUser = 'user'
+mongoPass = 'password'
+mongoHost = 'hostname'
+
+# Settings
+modLog = int
+adminChannel = int
+category = int
+guild = int
+modRole = int
+logUrl = 'webserver url'
