ae5246250d40955ff739fd43c708a1a4e29091c9
Matthew Cohen <matt@mattbsg.xyz>
2020-06-18 02:07:18 -0400

    Create example config

---
+++ config.example.py
@@ -0,0 +1,15 @@
+# Discord
+token = 'token'
+
+#Mongo Credentials
+mongoUser = 'user'
+mongoPass = 'password'
+mongoHost = 'host'
+
+# Settings
+modLog = int: mod log channel id
+adminChannel = int: admin channel id
+category = int: modmail category id
+guild = int: modmail guild id
+modRole = int: moderator role id
+logUrl = 'https://example.com/logs'
