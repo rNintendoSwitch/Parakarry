f471aef1a9dbea956a2324ba2011182cdac498f2
Matthew Cohen <matt@mattbsg.xyz>
2020-09-03 20:03:40 -0400

    Update example config

--- config.example.py
+++ config.example.py
@@ -12,5 +12,7 @@ modLog = int: mod log channel id
 adminChannel = int: admin channel id
 category = int: modmail category id
 guild = int: modmail guild id
+appealGuild = int: ban appeal guild id
 modRole = int: moderator role id
 logUrl = 'https://example.com/logs'
+appealInvite = 'https://discord.gg/invite'
