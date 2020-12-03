f94752dc9d5cf13aacd554950591ee37c291a6d5
Ian M <theianm2002@gmail.com>
2020-12-03 11:53:51 -0500

    Ignore config.example.py from pylance, cleanup

--- config.example.py
+++ config.example.py
@@ -1,18 +1,28 @@
-# Discord
+# type: ignore
+
+# Discord.py
 token = 'token'
 command_prefixes = ['!', ',', 'p']
 
-#Mongo Credentials
+# Mongo Credentials
 mongoUser = 'user'
 mongoPass = 'password'
 mongoHost = 'host'
 
-# Settings
+# Channel IDs
 modLog = int: mod log channel id
 adminChannel = int: admin channel id
+
+# Category ID
 category = int: modmail category id
+
+# Guild IDs
 guild = int: modmail guild id
 appealGuild = int: ban appeal guild id
+
+# Role ID
 modRole = int: moderator role id
+
+# URLs
 logUrl = 'https://example.com/logs'
 appealInvite = 'https://discord.gg/invite'
