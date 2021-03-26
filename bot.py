a2d7e9c1b9cdf3af6460568aaa0fb2e8fb8df6c1
Matthew Cohen <matt@mattbsg.xyz>
2021-03-25 22:08:06 -0400

    Revert "Fix AttributeError for command processing"
    This reverts commit 4dcec8c3b9ddae01e654a098670d72572d03f1e3. Unneeded in this function, as it itself is a command

--- bot.py
+++ bot.py
@@ -258,11 +258,7 @@ class Mail(commands.Cog):
     @commands.has_any_role(config.modRole)
     @commands.group(name='appeal', case_insensitive=True, invoke_without_command=True)
     async def _appeal(self, ctx):
-        if ctx.command:
-            cmd_str = ctx.command.full_parent_name + ' ' + ctx.command.name if ctx.command.parent else ctx.command.name
-        else:
-            cmd_str = '<command>'
-
+        cmd_str = ctx.command.full_parent_name + ' ' + ctx.command.name if ctx.command.parent else ctx.command.name
         await ctx.send(f':x: Incorrect usage. See `{ctx.prefix}help {cmd_str}`')
 
     @commands.has_any_role(config.modRole)
