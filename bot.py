fe18c6685812186e2a303c82ca28f09f05e06e6f
Matthew Cohen <matt@mattbsg.xyz>
2021-07-03 23:01:09 -0400

    Remove commands options for `content`

--- bot.py
+++ bot.py
@@ -237,26 +237,21 @@ class Mail(commands.Cog):
                 description='The user to start a thread with',
                 option_type=SlashCommandOptionType.USER,
                 required=True,
-            ),
-            create_option(
-                name='content',
-                description='The message to send to the user',
-                option_type=SlashCommandOptionType.STRING,
-                required=True,
-            ),
+            )
         ],
     )
     async def _open_thread(self, ctx: SlashContext, member, *, content):
         """
         Open a modmail thread with a user
         """
+        await ctx.defer()
         if mclient.modmail.logs.find_one({'recipient.id': str(member.id), 'open': True}):
             return await ctx.send(
                 ':x: Unable to open modmail to user -- there is already a thread involving them currently open'
             )
 
         try:
-            await utils._trigger_create_thread(
+            await utils._trigger_create_mod_thread(
                 self.bot,
                 member,
                 ctx.message,
@@ -282,26 +277,21 @@ class Mail(commands.Cog):
                 description='The user to start a thread with',
                 option_type=SlashCommandOptionType.USER,
                 required=True,
-            ),
-            create_option(
-                name='content',
-                description='The message to send to the user',
-                option_type=SlashCommandOptionType.STRING,
-                required=True,
-            ),
+            )
         ],
     )
     async def _open_thread_anon(self, ctx: SlashContext, member, *, content):
         """
         Open a modmail thread with a user anonymously
         """
+        await ctx.defer()
         if mclient.modmail.logs.find_one({'recipient.id': str(member.id), 'open': True}):
             return await ctx.send(
                 ':x: Unable to open modmail to user -- there is already a thread involving them currently open'
             )
 
         try:
-            await utils._trigger_create_user_thread(
+            await utils._trigger_create_mod_thread(
                 self.bot,
                 member,
                 ctx.message,
