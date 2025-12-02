0ff1853444e2a64f176720cf2b1fe0fba511cbde
Ian M. <6500391+7bitlyrus@users.noreply.github.com>
2025-12-02 18:07:49 -0500

    Fix check for perma ban appeal denials
    Also update language.

--- cogs/modmail.py
+++ cogs/modmail.py
@@ -422,11 +422,11 @@ class Mail(commands.Cog):
             else:
                 if (
                     self.leadModRole not in interaction.user.roles
-                    and punsDB.count_documents({'user': user.id, 'type': 'appealdeny'}) < 2
+                    and punsDB.count_documents({'user': user.id, 'type': 'appealdeny'}) <= 2
                 ):
                     # User has not met the minimum appeal denials to be permanently denied
                     return await interaction.response.send_message(
-                        ':x: To permanently deny a ban appeal, the user must have been denied at least 2 times previously'
+                        ':x: To permanently deny a ban appeal, the user must have at least 2 previously denied appeals. A Moderation Team Lead can bypass this restriction'
                     )
 
                 humanizedTimestamp = 'permanently'
